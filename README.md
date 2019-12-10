8e807f3df9ed492e9539d9898e477a08f6471be0
Kkla <sirawit14@gmail.com>
2019-12-10 11:51:49 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## install nodemon
+
+```
+sudo npm i -g nodemon
+```
+
+## Start Dev Project
+
+```
+npm run dev
+```
+
+## Lib for test
+
+```
+npm i chai-http mocha chai --save-dev
+```
+
+## Test Script
+
+```
+npm run test
+```
+
+## Install Koa Generator
+
+```
+sudo npm i -g koa-generator
+```
+
+```
+https://www.npmjs.com/package/koa-generator
+```
---
+++ app.js
@@ -0,0 +1,34 @@
+import Koa from "koa";
+import KoaRouter from "koa-router";
+import KoaJson from "koa-json";
+import "@babel/polyfill";
+import bodyParser from "koa-bodyparser";
+
+const port = 3100;
+const app = new Koa();
+const router = new KoaRouter();
+const api = require("./api/api");
+// Use as middleware
+app.use(bodyParser());
+app.use(KoaJson());
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+router.get("/", ctx => (ctx.body = { text: "Hello World" }));
+
+app.use(router.routes()).use(router.allowedMethods());
+app.use(api.routes(), api.allowedMethods());
+app.listen(port, () => console.log(`Server Running at port: ${port}`));
